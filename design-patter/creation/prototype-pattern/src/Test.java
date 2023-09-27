79cf9c4442444d31e7ddab405e6c7ab28869a0ac
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-27 16:22:06 +0700

    [design pattern] - prototype pattern - copy with new operator

---
+++ design-patter/creation/prototype-pattern/src/Test.java
@@ -0,0 +1,5 @@
+package PACKAGE_NAME;/**
+ * @author manhdt14
+ * created in 9/27/2023 3:28 PM
+ */public class Test {
+}
---
+++ design-patter/creation/prototype-pattern/src/cloneableinterface/Tree.java
@@ -0,0 +1,5 @@
+package cloneableinterface;/**
+ * @author manhdt14
+ * created in 9/27/2023 3:52 PM
+ */public class Tree {
+}
