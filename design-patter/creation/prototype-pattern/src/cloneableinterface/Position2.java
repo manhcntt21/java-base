0d63a320fb652137054d441d7a1f6d1260d0f80b
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-27 16:22:49 +0700

    [design pattern] - prototype pattern - copy with cloneable interface - default shallow copy

--- design-patter/creation/prototype-pattern/src/cloneableinterface/Position2.java
+++ design-patter/creation/prototype-pattern/src/cloneableinterface/Position2.java
@@ -1,14 +1,22 @@
-package newoperator;
+package cloneableinterface;
 
 /**
  * @author manhdt14
  * created in 9/27/2023 3:22 PM
  */
-public class Position {
-    private final int x;
-    private final int y;
+public class Position2 {
+    private  int x;
+    private  int y;
 
-    public Position(int x, int y) {
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Position2(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -38,7 +46,7 @@ public class Position {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Position other = (Position) obj;
+        Position2 other = (Position2) obj;
         if (x != other.x)
             return false;
         if (y != other.y)
--- design-patter/creation/prototype-pattern/src/cloneableinterface/Tree.java
+++ design-patter/creation/prototype-pattern/src/cloneableinterface/Tree.java
@@ -1,5 +1,53 @@
-package cloneableinterface;/**
+package cloneableinterface;
+
+import newoperator.Position;
+
+/**
  * @author manhdt14
  * created in 9/27/2023 3:52 PM
- */public class Tree {
+ */
+public class Tree implements Cloneable {
+    private double mass;
+    private double height;
+    private Position2 position;
+
+    public Tree(double mass, double height) {
+        this.mass = mass;
+        this.height = height;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setPosition(Position2 position) {
+        this.position = position;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public Position2 getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree [mass=" + mass + ", height=" + height + ", position=" + position + "]";
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+
+        return super.clone();
+    }
 }
